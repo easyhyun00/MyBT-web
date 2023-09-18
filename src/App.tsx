ce9f17ffe456e4e0e45934a9f79eb17ba196b1c1
이지현 <leejh@ijihyeon-ui-MacBookAir.local>
2023-09-18 22:00:34 +0900

    :tada: Init 프로젝트 초기 세팅 React, typeScript, Emotion, eslint, prettier 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import './fonts/fonts.css';
+import MainPage from './pages/main';
+import InfoPage from './pages/info';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* 웹 서비스 소개 페이지 */}
+        <Route path="/" element={<InfoPage />} />
+        {/* 메인 페이지 */}
+        <Route path="/main" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
